b5b08ef5d04162bf36ee6d76850df20aa0b82dfa
Cleverson Sampaio <cleverson@sampaio.dev.br>
2022-07-25 02:04:44 -0300

    feat: html writing script

---
+++ index.ts
@@ -0,0 +1,36 @@
+import { encode } from "he";
+import * as sanitizeXMLCharacters from "sanitize-xml-string";
+
+export default html;
+export type HTML = string;
+export function html(
+  template: TemplateStringsArray,
+  ...substitutions: (string | string[])[]
+): HTML {
+  const templatePartsSanitized = template.map((templatePart) =>
+    sanitizeXMLCharacters.sanitize(templatePart)
+  );
+  const substitutionsSanitized = [
+    ...substitutions.map((substitution) =>
+      (Array.isArray(substitution) ? substitution : [substitution]).map(
+        (substitutionPart) =>
+          sanitizeXMLCharacters.sanitize(String(substitutionPart))
+      )
+    ),
+    [],
+  ];
+  const buffer = new Array<string>();
+  for (const index of templatePartsSanitized.keys()) {
+    let templatePart = templatePartsSanitized[index];
+    let substitution = substitutionsSanitized[index];
+    if (templatePart.endsWith("$")) {
+      templatePart = templatePart.slice(0, -1);
+    } else {
+      substitution = substitution.map((substitutionPart) =>
+        encode(substitutionPart)
+      );
+    }
+    buffer.push(templatePart, ...substitution);
+  }
+  return buffer.join("");
+}
